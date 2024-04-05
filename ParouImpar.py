ca5ad257e3a717e416771e40452f4a49c1f8a214
Arielly09 <162440483+Arielly09@users.noreply.github.com>
2024-04-05 07:30:25 -0300

    Add files via upload

---
+++ ParouImpar.py
@@ -0,0 +1,6 @@
+num = int (input ("Digite um número e saberá se é impar ou par: "))
+
+if num % 2 == 0:
+    print ("\npar")
+
+else: print("impar")
---
+++ calculadora.py
@@ -0,0 +1,12 @@
+numero1= int(input( "insira o primero numero" ))
+numero2= int( input( "insira o segundo numero" ))
+
+adicao=numero1+numero2
+subtracao=numero1-numero2
+mutiplicacao=numero1*numero2
+divisao=numero1/numero2
+
+print(" a soma dos numeros é " ,adicao)
+print(" a subtracao dos numeros é " ,subtracao)
+print(" a multiplicacao dos numeros é " ,mutiplicacao)
+print(" a divivisao dos numeros é " ,divisao)
